16d4361e8fe37120dc263bab372b006ad4582f08
DegenBuilder <94379406+degenbuilder7@users.noreply.github.com>
2024-04-29 11:47:28 +0530

    UI

---
+++ frontend/config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Scroll Defi Manager",
+  description:
+    "Scroll Defi Manager.",
+  mainNav: [
+    {
+      title: "Dashboard",
+      href: "/dashboard",
+    }
+  ],
+  links: {
+    twitter: "https://twitter.com",
+  },
+}
